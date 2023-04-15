164282d03aa207f28577d747ed791475aada812a
PatelVed671 <118421398+PatelVed671@users.noreply.github.com>
2023-04-15 18:39:12 +0530

    vp | 15-04-2023 | 18:32 | # resume builder app crate DAY-1/2/3/4

--- lib/views/screen/carrier_objective_page.dart
+++ lib/views/screen/carrier_objective_page.dart
@@ -29,37 +29,80 @@ class _carrier_objective_pageState extends State<carrier_objective_page> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(20),
-        child: Container(
-          height: s.height * 0.3,
-          width: s.width,
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(5),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Text(
-                  "Career Objective",
-                  style: titeltext,
-                ),
-                SizedBox(height: s.height*0.02,),
-                TextField(
-                  maxLines: 5,
-                  decoration: InputDecoration(
-                    hintText: "Descripation",
-                    border: OutlineInputBorder(borderSide: BorderSide(color: Colors.grey.shade300),),
-                    errorBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(3),
+        child: Column(
+          children: [
+            Container(
+              height: s.height * 0.3,
+              width: s.width,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Career Objective",
+                      style: titeltext,
                     ),
-                  ),
+                    SizedBox(height: s.height*0.02,),
+                    TextField(
+                      maxLines: 5,
+                      decoration: InputDecoration(
+                        hintText: "Descripation",
+                        hintStyle: TextStyle(color: Colors.grey.shade400,fontSize: 22),
+                        border: OutlineInputBorder(borderSide: BorderSide(color: Colors.grey.shade300),),
+                        errorBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(3),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-              ],
+              ),
             ),
-          ),
+            SizedBox(height: s.height*0.02,),
+            Container(
+              height: s.height * 0.22,
+              width: s.width,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    const Text(
+                      "Current Designation (Experirnced\nCandidate ",
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: s.height*0.02,),
+                    TextField(
+                      maxLines: 1,
+                      decoration: InputDecoration(
+                        hintText: "Software Engineer",
+                        hintStyle: TextStyle(color: Colors.grey.shade400,fontSize: 20),
+                        border: OutlineInputBorder(borderSide: BorderSide(color: Colors.grey.shade300),),
+                        errorBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(3),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
       backgroundColor: Colors.grey.shade300,
