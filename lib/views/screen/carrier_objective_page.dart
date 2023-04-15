caa80ed46f7035e7bf3ec6716076b28a31639dd7
PatelVed671 <118421398+PatelVed671@users.noreply.github.com>
2023-04-15 19:17:30 +0530

    vp | 15-04-2023 | 18:32 | # resume builder app crate DAY-1/2/3/4

--- lib/views/screen/carrier_objective_page.dart
+++ lib/views/screen/carrier_objective_page.dart
@@ -48,13 +48,22 @@ class _carrier_objective_pageState extends State<carrier_objective_page> {
                       "Career Objective",
                       style: titeltext,
                     ),
-                    SizedBox(height: s.height*0.02,),
+                    SizedBox(
+                      height: s.height * 0.02,
+                    ),
                     TextField(
                       maxLines: 5,
                       decoration: InputDecoration(
                         hintText: "Descripation",
-                        hintStyle: TextStyle(color: Colors.grey.shade400,fontSize: 22),
-                        border: OutlineInputBorder(borderSide: BorderSide(color: Colors.grey.shade300),),
+                        hintStyle: TextStyle(
+                          color: Colors.grey.shade400,
+                          fontSize: 22,
+                        ),
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.grey.shade300,
+                          ),
+                        ),
                         errorBorder: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(3),
                         ),
@@ -64,7 +73,9 @@ class _carrier_objective_pageState extends State<carrier_objective_page> {
                 ),
               ),
             ),
-            SizedBox(height: s.height*0.02,),
+            SizedBox(
+              height: s.height * 0.02,
+            ),
             Container(
               height: s.height * 0.22,
               width: s.width,
@@ -86,13 +97,18 @@ class _carrier_objective_pageState extends State<carrier_objective_page> {
                         fontWeight: FontWeight.bold,
                       ),
                     ),
-                    SizedBox(height: s.height*0.02,),
+                    SizedBox(
+                      height: s.height * 0.02,
+                    ),
                     TextField(
                       maxLines: 1,
                       decoration: InputDecoration(
                         hintText: "Software Engineer",
-                        hintStyle: TextStyle(color: Colors.grey.shade400,fontSize: 20),
-                        border: OutlineInputBorder(borderSide: BorderSide(color: Colors.grey.shade300),),
+                        hintStyle: TextStyle(
+                            color: Colors.grey.shade400, fontSize: 20),
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.grey.shade300),
+                        ),
                         errorBorder: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(3),
                         ),
