b4c8c9e1a5cd8f4393c8cdbe455212158563bfb4
PatelVed671 <118421398+PatelVed671@users.noreply.github.com>
2023-04-15 19:35:23 +0530

    vp | 15-04-2023 | 19:35 | # resume builder app crate DAY-1/2/3/4

--- lib/views/screen/educyion_page.dart
+++ lib/views/screen/educyion_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_resume_builder_app/utils/back_button_icon.dart';
 import 'package:flutter_resume_builder_app/utils/theme_utils.dart';
 
 class educyion_page extends StatefulWidget {
@@ -11,12 +12,101 @@ class educyion_page extends StatefulWidget {
 class _educyion_pageState extends State<educyion_page> {
   @override
   Widget build(BuildContext context) {
+    Size s = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
+        leading: GestureDetector(
+            onTap: () {
+              setState(() {
+                Navigator.of(context).pop();
+              });
+            },
+            child: backicon),
+        toolbarHeight: s.height * 0.15,
         title: Text("Educyion", style: AppBarTitile),
         centerTitle: true,
         backgroundColor: Colors.blue,
       ),
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Container(
+          padding: EdgeInsets.all(20),
+          height: s.height * 0.6,
+          width: s.width,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(5),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(5),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Course/Degree",
+                  style: titeltext,
+                ),
+                SizedBox(height: s.height * 0.01),
+                TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    hintText: "B.Tech Information Technology",
+                    hintStyle: TextStyle(
+                      fontSize: 18,
+                      color: Colors.grey.shade400,
+                    ),
+                  ),
+                ),
+                SizedBox(height: s.height * 0.01),
+                Text("School/College/Institute", style: titeltext),
+                SizedBox(height: s.height * 0.01),
+                TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: "Bhagavan Mahavir University",
+                    hintStyle: TextStyle(
+                      fontSize: 18,
+                      color: Colors.grey.shade400,
+                    ),
+                  ),
+                ),
+                SizedBox(height: s.height * 0.01),
+                Text("School/College/Institute", style: titeltext),
+                SizedBox(height: s.height * 0.01),
+                TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: "70% (or) 7.0 CGPA",
+                    hintStyle: TextStyle(
+                      fontSize: 18,
+                      color: Colors.grey.shade400,
+                    ),
+                  ),
+                ),
+                SizedBox(height: s.height * 0.01),
+                Text(
+                  "Year Of Pass",
+                  style: titeltext,
+                ),
+                SizedBox(height: s.height * 0.01),
+                TextField(
+                  decoration: InputDecoration(
+                    border: const OutlineInputBorder(),
+                    hintText: "2019",
+                    hintStyle: TextStyle(
+                      color: Colors.grey.shade400,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      backgroundColor: Colors.grey.shade300,
     );
   }
 }
