6a7fe0ff82ae96bec9a621d86704df19cf5af3df
yashguard2002 <yashguard2002@gmail.com>
2023-04-24 11:32:39 +0530

    VP | 24-04-2023 | # Add Interest/Hobbies page

--- lib/views/screen/interest_hobbies_page.dart
+++ lib/views/screen/interest_hobbies_page.dart
@@ -24,6 +24,87 @@ class _interest_hobbies_pageState extends State<interest_hobbies_page> {
         centerTitle: true,
         backgroundColor: Colors.blue,
       ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Container(
+          height: s.height * 0.4,
+          width: s.width,
+          decoration: const BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.all(
+              Radius.circular(5),
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                // mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: s.height * 0.02,
+                  ),
+                  const Text(
+                    "Enter Your Hobbies",
+                    style: TextStyle(
+                      fontSize: 22,
+                      color: Colors.grey,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(
+                    height: s.height * 0.03,
+                  ),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      contentPadding: EdgeInsets.only(left: 20),
+                      hintText: "C Programming, Web Technical",
+                      hintStyle: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: s.height * 0.02,
+                  ),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      contentPadding: EdgeInsets.only(left: 20),
+                      hintText: "C Programming, Web Technical",
+                      hintStyle: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: s.height * 0.04,
+                  ),
+                  Container(
+                    height: s.height * 0.056,
+                    width: s.width,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(
+                        color: Colors.grey.shade300,
+                        width: 2,
+                      ),
+                    ),
+                    child: Icon(
+                      Icons.add,
+                      color: Colors.grey.shade300,
+                      size: s.height * 0.05,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+      backgroundColor: Colors.grey.shade300,
     );
   }
 }
